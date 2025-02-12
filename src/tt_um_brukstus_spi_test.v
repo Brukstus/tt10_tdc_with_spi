5a5f07f109c21f5d515e023e27b3340b0c31a97f
Brukstus <t.brukstus@gmail.com>
2025-02-12 20:23:47 +0200

    Changed info.yaml content.

--- src/tt_um_brukstus_spi_test.v
+++ src/tt_um_brukstus_spi_test.v
@@ -6,7 +6,7 @@
 
 `default_nettype none
 
-module tt_um_mattvenn_spi_test (
+module tt_um_brukstus_tdc_with_spi (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
